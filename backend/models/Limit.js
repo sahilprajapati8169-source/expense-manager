4fd39cc412e1877d6e83bd5a766bcc32a7e914d0
sahilprajapati8169-source <sahiprajapati8169@gmail.com>
2026-02-26 15:28:06 +0530

    fix limits api

--- backend/models/Limit.js
+++ backend/models/Limit.js
@@ -1,29 +1,20 @@
 const mongoose = require("mongoose");
 
-const limitSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-      unique: true
-    },
-
-    totalLimit: {
-      type: Number,
-      required: true
-    },
-
-    categoryLimits: {
-      Food: { type: Number, default: 0 },
-      Travel: { type: Number, default: 0 },
-      Rent: { type: Number, default: 0 },
-      Shopping: { type: Number, default: 0 },
-      Bills: { type: Number, default: 0 },
-      Others: { type: Number, default: 0 }
-    }
+const limitSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true
   },
-  { timestamps: true }
-);
+  totalLimit: {
+    type: Number,
+    default: 0
+  },
+  categoryLimits: {
+    type: Object,
+    default: {}
+  }
+});
 
 module.exports = mongoose.model("Limit", limitSchema);
